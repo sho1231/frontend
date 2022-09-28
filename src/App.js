b453d3d3036eac73fe6709a751bbec3d82617b39
shourja ganguly <shourja.ganguly@konnectbox.in>
2022-09-28 17:35:41 +0530

    Added readme link in app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Reset from './Components/Reset';
 function App() {
   return (
     <>
-      <div>For registering and login..please do it with the help of postman or insomnia by the reading the readme.txt file</div>
+      <div>For registering and login..please do it with the help of postman or insomnia by the reading the readme.txt file...<a href="https://github.com/sho1231/PassResetBackEnd/blob/master/readme.txt" target="_blank">click here to read readme.txt</a></div>
       <Routes>
         <Route exact path="/" element={<Navigate end to="/resetPage" />} />
         <Route exact path="/resetPage" element={<SendEmail />} />
